2def9ab355bd7080e6cd6a339fde69cb18e3a8a0
deerand <dwiyudirayia789@gmail.com>
2020-09-09 01:39:59 +0700

    setup packges

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import 'core-js/stable'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import CoreuiVue from '@coreui/vue'
+import { iconsSet as icons } from './assets/icons/icons.js'
+import { extend } from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import store from './store'
+
+Vue.config.performance = true
+Vue.use(CoreuiVue)
+Vue.prototype.$log = console.log.bind(console)
+
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule])
+})
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
+
+Vue.use(VueSweetalert2);
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  icons,
+  template: '<App/>',
+  components: {
+    App
+  }
+})
