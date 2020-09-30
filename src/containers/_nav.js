2859e54cc7f95a9698b93b9d6eb3f8e5cf9269da
Dwi Yudi <dwiyudirayia789@gmail.com>
2020-09-30 23:07:42 +0700

    Membuat Halaman Ganti Password

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -1,256 +1,265 @@
 export default [
-  {
-    _name: 'CSidebarNav',
-    _children: [
-      {
-        _name: 'CSidebarNavItem',
-        name: 'Beranda',
-        to: '/dashboard',
-        icon: 'cil-speedometer',
-      },
-      {
-        _name: 'CSidebarNavTitle',
-        _children: ['Menu']
-      },
-      {
-        _name: 'CSidebarNavDropdown',
-        name: 'Laporan',
-        route: '/admin/report',
-        icon: 'cil-pencil',
-        items: [
-          {
-            name: 'Baru',
-            to: '/admin/report/new',
-          },
-          {
-            name: 'Draft',
-            to: '/admin/report/draft'
-          },
-          {
-            name: 'Ditolak',
-            to: '/admin/report/reject'
-          },
-          {
-            name: 'Selesai',
-            to: '/admin/report/finished'
-          },
+    {
+        _name: 'CSidebarNav',
+        _children: [
+        {
+            _name: 'CSidebarNavItem',
+            name: 'Beranda',
+            to: '/dashboard',
+            icon: 'cil-speedometer',
+        },
+        {
+            _name: 'CSidebarNavTitle',
+            _children: ['Menu']
+        },
+        {
+            _name: 'CSidebarNavDropdown',
+            name: 'Laporan',
+            route: '/admin/report',
+            icon: 'cil-pencil',
+            items: [
+                {
+                    name: 'Baru',
+                    to: '/admin/report/new',
+                },
+                {
+                    name: 'Draft',
+                    to: '/admin/report/draft'
+                },
+                {
+                    name: 'Ditolak',
+                    to: '/admin/report/reject'
+                },
+                {
+                    name: 'Selesai',
+                    to: '/admin/report/finished'
+                },
+            ]
+        },
+        {
+            _name: 'CSidebarNavDropdown',
+            name: 'Akun',
+            icon: 'cil-people',
+            items: [
+                {
+                    name: 'Daftar Akun',
+                    to: '/admin/account',
+                },
+                {
+                    name: 'Ganti Password',
+                    to: '/admin/account/change/password',
+                },
+            ]
+        },
+        // {
+        //   _name: 'CSidebarNavTitle',
+        //   _children: ['Components']
+        // },
+        // {
+        //   _name: 'CSidebarNavDropdown',
+        //   name: 'Base',
+        //   route: '/base',
+        //   icon: 'cil-puzzle',
+        //   items: [
+        //     {
+        //       name: 'Breadcrumbs',
+        //       to: '/base/breadcrumbs'
+        //     },
+        //     {
+        //       name: 'Cards',
+        //       to: '/base/cards'
+        //     },
+        //     {
+        //       name: 'Carousels',
+        //       to: '/base/carousels'
+        //     },
+        //     {
+        //       name: 'Collapses',
+        //       to: '/base/collapses'
+        //     },
+        //     {
+        //       name: 'Forms',
+        //       to: '/base/forms'
+        //     },
+        //     {
+        //       name: 'Jumbotrons',
+        //       to: '/base/jumbotrons'
+        //     },
+        //     {
+        //       name: 'List Groups',
+        //       to: '/base/list-groups'
+        //     },
+        //     {
+        //       name: 'Navs',
+        //       to: '/base/navs'
+        //     },
+        //     {
+        //       name: 'Navbars',
+        //       to: '/base/navbars'
+        //     },
+        //     {
+        //       name: 'Paginations',
+        //       to: '/base/paginations'
+        //     },
+        //     {
+        //       name: 'Popovers',
+        //       to: '/base/popovers'
+        //     },
+        //     {
+        //       name: 'Progress Bars',
+        //       to: '/base/progress-bars'
+        //     },
+        //     {
+        //       name: 'Switches',
+        //       to: '/base/switches'
+        //     },
+        //     {
+        //       name: 'Tables',
+        //       to: '/base/tables'
+        //     },
+        //     {
+        //       name: 'Tabs',
+        //       to: '/base/tabs'
+        //     },
+        //     {
+        //       name: 'Tooltips',
+        //       to: '/base/tooltips'
+        //     }
+        //   ]
+        // },
+        // {
+        //   _name: 'CSidebarNavDropdown',
+        //   name: 'Buttons',
+        //   route: '/buttons',
+        //   icon: 'cil-cursor',
+        //   items: [
+        //     {
+        //       name: 'Buttons',
+        //       to: '/buttons/standard-buttons'
+        //     },
+        //     {
+        //       name: 'Button Dropdowns',
+        //       to: '/buttons/dropdowns'
+        //     },
+        //     {
+        //       name: 'Button Groups',
+        //       to: '/buttons/button-groups'
+        //     },
+        //     {
+        //       name: 'Brand Buttons',
+        //       to: '/buttons/brand-buttons'
+        //     }
+        //   ]
+        // },
+        // {
+        //   _name: 'CSidebarNavItem',
+        //   name: 'Charts',
+        //   to: '/charts',
+        //   icon: 'cil-chart-pie'
+        // },
+        // {
+        //   _name: 'CSidebarNavDropdown',
+        //   name: 'Icons',
+        //   route: '/icons',
+        //   icon: 'cil-star',
+        //   items: [
+        //     {
+        //       name: 'CoreUI Icons',
+        //       to: '/icons/coreui-icons',
+        //       badge: {
+        //         color: 'info',
+        //         text: 'NEW'
+        //       }
+        //     },
+        //     {
+        //       name: 'Brands',
+        //       to: '/icons/brands'
+        //     },
+        //     {
+        //       name: 'Flags',
+        //       to: '/icons/flags'
+        //     }
+        //   ]
+        // },
+        // {
+        //   _name: 'CSidebarNavDropdown',
+        //   name: 'Notifications',
+        //   route: '/notifications',
+        //   icon: 'cil-bell',
+        //   items: [
+        //     {
+        //       name: 'Alerts',
+        //       to: '/notifications/alerts'
+        //     },
+        //     {
+        //       name: 'Badges',
+        //       to: '/notifications/badges'
+        //     },
+        //     {
+        //       name: 'Modals',
+        //       to: '/notifications/modals'
+        //     }
+        //   ]
+        // },
+        // {
+        //   _name: 'CSidebarNavItem',
+        //   name: 'Widgets',
+        //   to: '/widgets',
+        //   icon: 'cil-calculator',
+        //   badge: {
+        //     color: 'primary',
+        //     text: 'NEW',
+        //     shape: 'pill'
+        //   }
+        // },
+        // {
+        //   _name: 'CSidebarNavDivider',
+        //   _class: 'm-2'
+        // },
+        // {
+        //   _name: 'CSidebarNavTitle',
+        //   _children: ['Extras']
+        // },
+        // {
+        //   _name: 'CSidebarNavDropdown',
+        //   name: 'Pages',
+        //   route: '/pages',
+        //   icon: 'cil-star',
+        //   items: [
+        //     {
+        //       name: 'Login',
+        //       to: '/pages/login'
+        //     },
+        //     {
+        //       name: 'Register',
+        //       to: '/pages/register'
+        //     },
+        //     {
+        //       name: 'Error 404',
+        //       to: '/pages/404'
+        //     },
+        //     {
+        //       name: 'Error 500',
+        //       to: '/pages/500'
+        //     }
+        //   ]
+        // },
+        // {
+        //   _name: 'CSidebarNavItem',
+        //   name: 'Download CoreUI',
+        //   href: 'http://coreui.io/vue/',
+        //   icon: { name: 'cil-cloud-download', class: 'text-white' },
+        //   _class: 'bg-success text-white',
+        //   target: '_blank'
+        // },
+        // {
+        //   _name: 'CSidebarNavItem',
+        //   name: 'Try CoreUI PRO',
+        //   href: 'http://coreui.io/pro/vue/',
+        //   icon: { name: 'cil-layers', class: 'text-white' },
+        //   _class: 'bg-danger text-white',
+        //   target: '_blank'
+        // }
         ]
-      },
-      {
-        _name: 'CSidebarNavItem',
-        name: 'Akun',
-        to: '/admin/account',
-        icon: 'cil-people'
-      },
-      // {
-      //   _name: 'CSidebarNavTitle',
-      //   _children: ['Components']
-      // },
-      // {
-      //   _name: 'CSidebarNavDropdown',
-      //   name: 'Base',
-      //   route: '/base',
-      //   icon: 'cil-puzzle',
-      //   items: [
-      //     {
-      //       name: 'Breadcrumbs',
-      //       to: '/base/breadcrumbs'
-      //     },
-      //     {
-      //       name: 'Cards',
-      //       to: '/base/cards'
-      //     },
-      //     {
-      //       name: 'Carousels',
-      //       to: '/base/carousels'
-      //     },
-      //     {
-      //       name: 'Collapses',
-      //       to: '/base/collapses'
-      //     },
-      //     {
-      //       name: 'Forms',
-      //       to: '/base/forms'
-      //     },
-      //     {
-      //       name: 'Jumbotrons',
-      //       to: '/base/jumbotrons'
-      //     },
-      //     {
-      //       name: 'List Groups',
-      //       to: '/base/list-groups'
-      //     },
-      //     {
-      //       name: 'Navs',
-      //       to: '/base/navs'
-      //     },
-      //     {
-      //       name: 'Navbars',
-      //       to: '/base/navbars'
-      //     },
-      //     {
-      //       name: 'Paginations',
-      //       to: '/base/paginations'
-      //     },
-      //     {
-      //       name: 'Popovers',
-      //       to: '/base/popovers'
-      //     },
-      //     {
-      //       name: 'Progress Bars',
-      //       to: '/base/progress-bars'
-      //     },
-      //     {
-      //       name: 'Switches',
-      //       to: '/base/switches'
-      //     },
-      //     {
-      //       name: 'Tables',
-      //       to: '/base/tables'
-      //     },
-      //     {
-      //       name: 'Tabs',
-      //       to: '/base/tabs'
-      //     },
-      //     {
-      //       name: 'Tooltips',
-      //       to: '/base/tooltips'
-      //     }
-      //   ]
-      // },
-      // {
-      //   _name: 'CSidebarNavDropdown',
-      //   name: 'Buttons',
-      //   route: '/buttons',
-      //   icon: 'cil-cursor',
-      //   items: [
-      //     {
-      //       name: 'Buttons',
-      //       to: '/buttons/standard-buttons'
-      //     },
-      //     {
-      //       name: 'Button Dropdowns',
-      //       to: '/buttons/dropdowns'
-      //     },
-      //     {
-      //       name: 'Button Groups',
-      //       to: '/buttons/button-groups'
-      //     },
-      //     {
-      //       name: 'Brand Buttons',
-      //       to: '/buttons/brand-buttons'
-      //     }
-      //   ]
-      // },
-      // {
-      //   _name: 'CSidebarNavItem',
-      //   name: 'Charts',
-      //   to: '/charts',
-      //   icon: 'cil-chart-pie'
-      // },
-      // {
-      //   _name: 'CSidebarNavDropdown',
-      //   name: 'Icons',
-      //   route: '/icons',
-      //   icon: 'cil-star',
-      //   items: [
-      //     {
-      //       name: 'CoreUI Icons',
-      //       to: '/icons/coreui-icons',
-      //       badge: {
-      //         color: 'info',
-      //         text: 'NEW'
-      //       }
-      //     },
-      //     {
-      //       name: 'Brands',
-      //       to: '/icons/brands'
-      //     },
-      //     {
-      //       name: 'Flags',
-      //       to: '/icons/flags'
-      //     }
-      //   ]
-      // },
-      // {
-      //   _name: 'CSidebarNavDropdown',
-      //   name: 'Notifications',
-      //   route: '/notifications',
-      //   icon: 'cil-bell',
-      //   items: [
-      //     {
-      //       name: 'Alerts',
-      //       to: '/notifications/alerts'
-      //     },
-      //     {
-      //       name: 'Badges',
-      //       to: '/notifications/badges'
-      //     },
-      //     {
-      //       name: 'Modals',
-      //       to: '/notifications/modals'
-      //     }
-      //   ]
-      // },
-      // {
-      //   _name: 'CSidebarNavItem',
-      //   name: 'Widgets',
-      //   to: '/widgets',
-      //   icon: 'cil-calculator',
-      //   badge: {
-      //     color: 'primary',
-      //     text: 'NEW',
-      //     shape: 'pill'
-      //   }
-      // },
-      // {
-      //   _name: 'CSidebarNavDivider',
-      //   _class: 'm-2'
-      // },
-      // {
-      //   _name: 'CSidebarNavTitle',
-      //   _children: ['Extras']
-      // },
-      // {
-      //   _name: 'CSidebarNavDropdown',
-      //   name: 'Pages',
-      //   route: '/pages',
-      //   icon: 'cil-star',
-      //   items: [
-      //     {
-      //       name: 'Login',
-      //       to: '/pages/login'
-      //     },
-      //     {
-      //       name: 'Register',
-      //       to: '/pages/register'
-      //     },
-      //     {
-      //       name: 'Error 404',
-      //       to: '/pages/404'
-      //     },
-      //     {
-      //       name: 'Error 500',
-      //       to: '/pages/500'
-      //     }
-      //   ]
-      // },
-      // {
-      //   _name: 'CSidebarNavItem',
-      //   name: 'Download CoreUI',
-      //   href: 'http://coreui.io/vue/',
-      //   icon: { name: 'cil-cloud-download', class: 'text-white' },
-      //   _class: 'bg-success text-white',
-      //   target: '_blank'
-      // },
-      // {
-      //   _name: 'CSidebarNavItem',
-      //   name: 'Try CoreUI PRO',
-      //   href: 'http://coreui.io/pro/vue/',
-      //   icon: { name: 'cil-layers', class: 'text-white' },
-      //   _class: 'bg-danger text-white',
-      //   target: '_blank'
-      // }
-    ]
-  }
+    }
 ]
