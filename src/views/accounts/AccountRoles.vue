6994701e8bcb29a7e3d9d09d7e6e49a42f4b0319
Dwi Yudi <dwiyudirayia789@gmail.com>
2020-10-14 17:46:28 +0700

    Mengambil response message di backend

--- src/views/accounts/AccountRoles.vue
+++ src/views/accounts/AccountRoles.vue
@@ -515,14 +515,11 @@ export default {
             const url = '/roles';
             const formData = new FormData();
             let urlAction = null;
-            let message = null;
             if (this.modal.post_put.method === 'patch') {
                 urlAction = `${url}/${this.forms.id}`;
-                message = 'Perbaharui';
                 formData.append('_method', 'patch');
             } else {
                 urlAction = url;
-                message = 'Tambah';
                 formData.append('_method', 'POST');
             }
             const forMapData = Object.entries(this.forms);
@@ -548,11 +545,11 @@ export default {
                 url: urlAction,
                 data: formData,
             })
-                .then(() => {
+                .then((response) => {
                     this.getData();
                     this.closeModalPostPut();
                     this.alert.show = true;
-                    this.alert.message = `Data Berhasil di ${message}`;
+                    this.alert.message = response.data.messages;
                     this.alert.style = 'success';
                     this.alert.counter = 3;
                 })
@@ -566,7 +563,7 @@ export default {
                     }
                     this.alert.show = true;
                     this.alert.style = 'danger';
-                    this.alert.message = `Data Gagal di ${message}`;
+                    this.alert.message = error.response.data.messages;
                     this.alert.counter = 3;
                 });
         },
